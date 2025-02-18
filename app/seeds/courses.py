05924f740566af82acb0ab3a425f80f1e064d19f
Mcode4 <lamllc20@gmail.com>
2025-02-18 16:35:47 -0500

    course seeds

---
+++ app/seeds/courses.py
@@ -0,0 +1,24 @@
+from app.models import db, Course, environment, SCHEMA
+from app.models.course import Category
+from sqlalchemy.sql import text
+
+def seed_courses():
+    demo = Course(
+        owner_id = 1,
+        name = 'Demo Course',
+        category = Category.FUN,
+        description = 'for test',
+        image= 'https://media.istockphoto.com/id/1400865154/photo/insurance-and-risk-management-concept.jpg?s=612x612&w=0&k=20&c=qYIonj5JRDaBZHtJ0oMXJTCazwI2bUw_T7NBFq4rjn4=',
+        rating = 5.0
+    )
+
+    db.session.add(demo)
+    db.session.commit()
+
+def undo_courses():
+    if environment == 'production':
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM users"))
+    
+    db.session.commit()
