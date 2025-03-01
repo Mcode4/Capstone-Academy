dc462ac3c5ef57afe9682fc7b6f514490e800313
Mcode4 <lamllc20@gmail.com>
2025-03-01 01:32:49 -0500

    directory fix

--- app/__init__.py
+++ app/__init__.py
@@ -92,11 +92,11 @@ def react_root(path):
     """
     if path == 'favicon.ico':
         return app.send_from_directory('public', 'favicon.ico')
-    if path == 'caLogo1t.png':
+    if path == '/public/images/caLogo1t.png':
         return app.send_from_directory('public', 'caLogo1t.png')
-    if path == 'footerImg2.png':
+    if path == '/public/images/footerImg2.png':
         return app.send_from_directory('public', 'footerImg2.png')
-    if path == 'coverImage2.png':
+    if path == '/public/images/coverImage2.png':
         return app.send_from_directory('public', 'coverImage2.png')
     
     return app.send_static_file('index.html')
